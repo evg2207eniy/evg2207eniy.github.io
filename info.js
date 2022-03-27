f103ac869fb32ff2ac1d8306a398d69ce5e9756c
evg2207eniy <102467243+evg2207eniy@users.noreply.github.com>
2022-03-27 17:44:52 +0600

    Add files via upload

---
+++ info.js
@@ -0,0 +1,21 @@
+var json = [{
+    "Cells": {
+        "geoData": {"type": "Point", "coordinates": [53.286697, 69.369622]}
+    }
+}, {
+    "Cells": {
+        "geoData": {"type": "Point", "coordinates": [53.287505, 69.369478]}
+    }
+}, {
+    "Cells": {
+        "geoData": {"type": "Point", "coordinates": [53.288786, 69.370125]}
+    }
+}, {
+    "Cells": {
+        "geoData": {"type": "Point", "coordinates": [53.288102, 69.373045]}
+    }
+}, {
+    "Cells": {
+        "geoData": {"type": "Point", "coordinates": [53.284991, 69.370170]}
+    }
+}]
